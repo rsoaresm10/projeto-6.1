5d2856e04d18a93ea595e21336d18e19e6dda22c
rafael <rafa100103soaresmendes@gmail.com>
2022-12-12 11:18:56 -0300

    add assets

--- js/router.js
+++ js/router.js
@@ -14,9 +14,15 @@ export class Router {
      handle (){
         const {pathname }= window.location
         const route = this.routes[pathname] || this.routes[404]
+        const urls = {
+            '/': './assets/mountains-universe-1.svg',
+            '/universe': './assets/mountains-universe-2.svg',
+            '/exploration': './assets/mountains-universe-3.svg',
+            404: './assets/mountains-universe-3.svg'
+          }
         fetch(route)
         .then(data => data.text())
         .then(html => {document.querySelector('#app').innerHTML = html})
-    console.log(pathname)
+        document.querySelector('.image').setAttribute('src', urls[pathname])
     }
     }
